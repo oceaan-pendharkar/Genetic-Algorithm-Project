a35bffc2909c88a27124062f62cf21867f1c1175
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2024-11-19 15:08:42 -0800

    Use doubles in city class.

--- city.hpp
+++ city.hpp
@@ -8,13 +8,24 @@ class city {
      * Coordinates: x  (longitude) and y (latitude)
      * x and y must be within the range [0, 1000] inclusive
      */
-    int x;
-    int y;
+    double x;
+    double y;
 
     /**
      * Constructor
      * @param x position on x-axis (i.e. longitude) of city, an int
      * @param y position on y-axis (i.e. latitude) of city, an int
      */
-    city(const int x, const int y):x(x), y(y){}
+    city(const double x, const double y):x(x), y(y){}
+
+    /**
+     * Get Distance Between Cities
+     *
+     * Gets the distance between 2 cities using the pythagorean distance formula d = sqrt((x2-x1)^2 + (yz-y1)^2)
+     * 
+     * @param a a city
+     * @param b another city
+     * @return the distance between the two cities
+     */
+    friend double get_distance_between_cities(city a, city b);
 };
